e80f239124e7d5cf4ec7e3585ad3dbbdc8914018
Zsolt F <zsoltfehervari@gmx.com>
2026-03-19 15:57:16 +0100

    Bump version to 1.1

--- hide-popups.user.js
+++ hide-popups.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Hide Popups GMX.com
 // @description     Hides the annoying popups on gmx.com
-// @version         1.0
+// @version         1.1
 // @author          0x7s0lt1 (https://github.com/0x7s0lt1/hide-popups-gmx-com)
 // @match           *://*.gmx.com/*
 // ==/UserScript==
